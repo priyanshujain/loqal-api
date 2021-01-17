21e68f22beb055feaaab1395769e2c2b7713b114
Priyanshu Jain <priyanshu@spotlightandcompany.com>
2021-01-17 14:35:16 +0530

    FIx sub category search

--- apps/merchant/dbapi/search.py
+++ apps/merchant/dbapi/search.py
@@ -12,8 +12,11 @@ def get_merchant_qs_by_category(merchant_qs, category):
 
 
 def merchant_search_by_keyword(merchant_qs, keyword):
+    category_keyword = keyword.replace(" ", "_").replace("-", "_")
+    sub_category_keyword = keyword.replace(" ", "-").replace("_", "-")
     return merchant_qs.filter(
         Q(profile__full_name__icontains=keyword)
-        | Q(categories__category__icontains=keyword)
+        | Q(categories__category__icontains=category_keyword)
         | Q(profile__about__icontains=keyword)
+        | Q(categories__sub_categories__icontains=sub_category_keyword)
     )
