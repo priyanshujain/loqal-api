3c3c36bbde8560fc46382a1acb002cc8927d8853
Priyanshu Jain <priyanshu@pm.me>
2021-01-22 18:14:49 +0530

    FIx

--- apps/account/options.py
+++ apps/account/options.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext as _
 
-from db.models.fields import ChoiceEnum
+from db.models.fields import ChoiceCharEnum, ChoiceEnum
 
 
 class MerchantAccountStatus(ChoiceEnum):
@@ -24,3 +24,28 @@ class ConsumerAccountStatus(ChoiceEnum):
     PENDING = 0, _("Pending")
     VERIFIED = 1, _("Verified")
     UNVERIFIED = 2, _("Unverified")
+
+
+class DwollaCustomerVerificationStatus(ChoiceCharEnum):
+    NOT_SENT = "not_sent", _("Not Sent")
+    VERIFIED = "verified", _("Verified")
+    UNVERIFIED = "unverified", _("Unverified")
+    DOCUMENT_NEEDED = "document_needed", _("Document Needed")
+    DOCUMENT_UPLOADED = "document_uploaded", _("Document Uploaded")
+    DOCUMENT_FAILED = "document_failed", _("Document Failed")
+    DOCUMENT_APPROVED = "document_approved", _("Document Approved")
+    REVERIFICATION_NEEDED = "reverification_needed", _("Reverification Needed")
+    RETRY = "retry", _("Retry")
+    SUSPENDED = "suspended", _("Suspended")
+    ACTIVATED = "activated", _("Activated")
+    DEACTIVATED = "deactivated", _("Deactivated")
+
+
+class DwollaCustomerStatus(ChoiceCharEnum):
+    NOT_SENT = "not_sent", _("Not Sent")
+    VERIFIED = "verified", _("Verified")
+    UNVERIFIED = "unverified", _("Unverified")
+    DOCUMENT = "document", _("Document")
+    RETRY = "retry", _("Retry")
+    SUSPENDED = "suspended", _("Suspended")
+    DEACTIVATED = "deactivated", _("Deactivated")
