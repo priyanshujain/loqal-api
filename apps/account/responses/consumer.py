a950a198dd11224b0843a358d0c329cabbba0856
Priyanshu Jain <priyanshu@pm.me>
2021-02-04 16:43:24 +0530

    Fix

--- apps/account/responses/consumer.py
+++ apps/account/responses/consumer.py
@@ -1,4 +1,5 @@
 from api import serializers
+from apps.account.dbapi import get_payment_account_consent
 from apps.account.models import ConsumerAccount
 
 __all__ = ("ConsumerAccountProfileResponse",)
@@ -6,6 +7,9 @@ __all__ = ("ConsumerAccountProfileResponse",)
 
 class ConsumerAccountProfileResponse(serializers.ModelSerializer):
     zip_code = serializers.CharField(source="account.zip_code", read_only=True)
+    payment_account_opening_consent = serializers.SerializerMethodField(
+        "get_payment_account_opening_consent"
+    )
 
     class Meta:
         model = ConsumerAccount
@@ -13,4 +17,13 @@ class ConsumerAccountProfileResponse(serializers.ModelSerializer):
             "created_at",
             "zip_code",
             "username",
+            "payment_account_opening_consent",
         )
+
+    def get_payment_account_opening_consent(self, obj):
+        consent = get_payment_account_consent(
+            account_id=obj.account.id, user_id=obj.user.id
+        )
+        if consent:
+            return True
+        return False
