cc31507dbdfba47d978ef23f8a28d018ef9bf50d
Priyanshu Jain <priyanshu@pm.me>
2021-03-10 15:18:37 +0530

    temp change phone number otp setting

--- apps/user/services/phone_number.py
+++ apps/user/services/phone_number.py
@@ -203,6 +203,13 @@ class EnrollSmsAuthenticator(object):
                 return True
             else:
                 return False
+        
+        if settings.APP_ENV == "staging":
+            if otp == "111111":
+                self._enroll_interface(interface=interface)
+                return True
+            else:
+                return False
 
         if interface.validate_otp(otp):
             self._enroll_interface(interface)
--- utils/sms.py
+++ utils/sms.py
@@ -16,16 +16,18 @@ def format_phone(phone_number, phone_number_country):
     )
 
 
-def send_sms(
-    body, phone_number, phone_number_country=settings.DEFAULT_PHONE_REGION
-):
+def send_sms(body, phone_number, phone_number_country=settings.DEFAULT_PHONE_REGION):
     phone_from = format_phone(
         settings.LOQAL_SMS_PHONE_NUMBER,
         settings.LOQAL_SMS_PHONE_NUMBER_COUNTRY,
     )
     phone_to = format_phone(phone_number, phone_number_country)
 
-    if settings.APP_ENV == "local" or settings.APP_ENV == "development":
+    if (
+        settings.APP_ENV == "local"
+        or settings.APP_ENV == "development"
+        or settings.APP_ENV == "staging"
+    ):
         print("###################### SMS START ########################")
         print("TO: ", phone_to)
         print("FROM: ", phone_from)
